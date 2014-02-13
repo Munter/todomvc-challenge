37c541f7d62255729519983f128ad7ab689f44dc
Peter Müller <munter@fumle.dk>
2014-02-13 23:48:36 +0100

    Updated Ember to prevent global leakage of 'use strict'

--- architecture-examples/emberjs/js/models/todo.js
+++ architecture-examples/emberjs/js/models/todo.js
@@ -1,11 +1,9 @@
 /*global Todos, DS */
-'use strict';
+(function () {
+	'use strict';
 
-Todos.Todo = DS.Model.extend({
-	title: DS.attr('string'),
-	isCompleted: DS.attr('boolean'),
-
-	saveWhenCompletedChanged: function () {
-		this.save();
-	}.observes('isCompleted')
-});
+	Todos.Todo = DS.Model.extend({
+		title: DS.attr('string'),
+		isCompleted: DS.attr('boolean')
+	});
+}());
